35fc8999c7e575cf9dfa7144e4c18e26159d057d
Nathan Berry <nberry@ptc.com>
2013-09-23 14:23:40 -0600

    Added icons to navbar items. Added contact info placeholder. Added headshot to navbar.

--- public/js/app.js
+++ public/js/app.js
@@ -5,5 +5,6 @@ angular.module('resume', [])
             .when('/skills', {templateUrl: 'views/skills.html', controller: SkillsController})
             .when('/work', {templateUrl: 'views/work.html', controller: WorkController})
             .when('/education', {templateUrl: 'views/education.html', controller: EducationController})
+            .when('/contact', {templateUrl: 'views/contact.html', controller: ContactController})
             .otherwise({redirectTo: '/'});
     }]);
